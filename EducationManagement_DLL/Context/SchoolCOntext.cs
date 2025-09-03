377bda1b771f6cf666d98b796bd07d93e191180d
Shahanaj <fashionfuel267@gmail.com>
2025-09-03 13:42:23 +0600

    Add project files.

---
+++ EducationManagement_DLL/Context/SchoolCOntext.cs
@@ -0,0 +1,103 @@
+﻿using EducationManagement_DLL.Models;
+using EducationManagement_DLL.Models.AccountsModel;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using RoyalAPI.Models.AccountModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationManagement_DLL.Context
+{
+    public class DbSchoolContextFactory : IDesignTimeDbContextFactory<SchoolCOntext>
+    {
+        public SchoolCOntext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<SchoolCOntext>();
+            optionsBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS;database=dbSchoolSAAS; Trusted_Connection=True;MultipleActiveResultSets=True;trustservercertificate=true;");
+
+            return new SchoolCOntext(optionsBuilder.Options);
+        }//User id=sa;password=123;
+    }
+    public class SchoolCOntext:IdentityDbContext<ApplicationUser>
+    {
+        
+        public SchoolCOntext(DbContextOptions<SchoolCOntext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Module>().Ignore(m => m.CustomAttributes);
+
+            //modelBuilder.Entity<CustomAttributeData>().HasNoKey();
+            base.OnModelCreating(modelBuilder);
+            // Additional model configurations can go here
+        }
+        public DbSet<Institute> Institutes { get; set; } = null!;
+        public DbSet<InstituteType> InstituteTypes { get; set; } = null!;
+        public DbSet<InsBranch> InsBranches { get; set; } = null!;
+        #region Academic Settings
+        public DbSet<AcademicSession> AcademicSessions { get; set; } = null!;
+        public DbSet<AcademicDepartment> AcademicDepartments { get; set; } = null!;
+        public DbSet<ProgramGroup> ProgramGroups { get; set; } = null!;
+        public DbSet<Program> Programs { get; set; } = null!;
+        public DbSet<AcademyClass> AcademyClasses { get; set; } = null!;
+        public DbSet<SubjectInfo> SubjectInfos { get; set; } = null!;
+        public DbSet<EducationalGroup> EducationalGroups { get; set; } = null!;
+        public DbSet<ClassSubjectInst> ClassWiseSubjects { get; set; } = null!;
+        public DbSet<Shift> Shifts { get; set; } = null!;
+        public DbSet<ClassSection> ClassSections { get; set; } = null!;
+        public DbSet<ClassSubjecTeacherIns> ClassSubjecTeacherIns { get; set; } = null!;
+        public DbSet<ShiftwiseClass> ShiftwiseClasses { get; set; } = null!;
+
+        #endregion
+
+        #region Employee Settings
+            public DbSet<Employee> Employees { get; set; } = null!;
+        public DbSet<AdministrativeDepartment> AdministrativeDepartments { get; set; } = null!;
+        public DbSet<Designation> Designations { get; set; } = null!;
+        public DbSet<Religion> Religions { get; set; } = null!;
+        public  DbSet<EmployeesGroup> EmployeesGroups { get; set; } = null!;
+        public DbSet<EmployeeWithGroup> EmployeeWithGroups { get; set; } = null!;
+        public DbSet<Directors> Directors { get; set; } = null!;
+        public DbSet<Staff> Staffs { get; set; } = null!;
+        public DbSet<Teacher> Teachers { get; set; } = null!;
+        public DbSet<EmployeeExperience> EmployeeExperiences { get; set; } = null!;
+        public DbSet<EmployeesEducation> EmployeesEducations { get; set; } = null!;
+        public  DbSet<EmployeeSalary> EmployeeSalaries { get; set; } = null!;
+        public DbSet<EmployeeQualification> EmployeeQualification { get; set; } = null!;
+        #endregion 
+        #region Accounts Settings
+        public DbSet<ShareHolderInformation> ShareHolderInformations { get; set; } = null!;
+        public DbSet<AccountChart> AccountCharts { get; set; } = null!;
+        public DbSet<AccountGroupTop> AccountGroupTops { get; set; } = null!;
+        public DbSet<AccountGroupMid> AccountGroupMides { get; set; } = null!;
+        public DbSet<AccountGroupLower> AccountGroupLowers { get; set; } = null!;
+        public DbSet<ClassWiseFee>ClassWiseFees { get; set; } = null!;
+        public DbSet<TransactionMaster> TransactionMasters { get; set; } = null!;
+        public DbSet<TransactionDetails> TransactionDetails { get; set; } = null!;
+        public DbSet<VoucherType> VoucherTypes { get; set; } = null!;
+        //public DbSet<ShareHolderTransaction> ShareHolderTransactions { get; set; } = null!;
+        //public DbSet<ShareHolderTransactionDetails> ShareHolderTransactionDetails { get; set; } = null!;
+        //    public DbSet<ShareHolderTransactionType> ShareHolderTransactionTypes { get; set; } = null!;
+        #endregion
+
+        #region Student Settings
+        public DbSet<StudentBasicInfo> StudentBasicInfos { get; set; } = null!;
+        public DbSet<StdtAcademicInfo> StdtAcademicInfos { get; set; } = null!;
+        #endregion
+
+        #region Student PAyment
+        public DbSet<StdPayment> StdPayments { get; set; } = null!;
+        public DbSet<StdInvoice> StdInvoices { get; set; } = null!;
+        public DbSet<StdPaymentSettings> StdPaymentSettings { get; set; } = null!;
+        public DbSet<PaymentMethod> PaymentMethods { get; set; } = null!;
+
+        #endregion
+
+    }
+}
